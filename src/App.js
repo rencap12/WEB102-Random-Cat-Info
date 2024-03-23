553ff7537ee3fbb4d91c79b40a66849e9e8d2a5a
rencap12 <rcapuno@ufl.edu>
2024-03-23 17:06:15 -0400

    finishin

--- src/App.js
+++ src/App.js
@@ -1,47 +1,98 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
-const ACCESS_KEY = process.env.REACT_APP_CAT_API_KEY;
+import './App.css';
 
 const App = () => {
+  const [catData, setCatData] = useState({
+    name: "",
+    weight: "",
+    origin: "",
+  });
   const [catImage, setCatImage] = useState('');
-  const [breedName, setBreedName] = useState('');
-  const [temperament, setTemperament] = useState('');
-  const [origin, setOrigin] = useState('');
 
   useEffect(() => {
-    const fetchcatImage = async () => {
+    const fetchCatData = async () => {
       try {
-        const response = await axios.get('https://api.thecatapi.com/v1/images/search?limit=10', {
+        const imageDataResponse = await axios.get('https://api.thecatapi.com/v1/images/search?&has_breeds=true&limit=1', {
           headers: {
-            "Content-Type" : "application/json",
-            "x-api-key": ACCESS_KEY
+            'Content-Type': 'application/json',
+            'x-api-key': process.env.REACT_APP_CAT_API_KEY
+          },
+        });
+
+        const imageData = imageDataResponse.data[0];
+        setCatImage(imageData);
+
+        if (!imageData) return;
+
+        const catDataResponse = await axios.get(`https://api.thecatapi.com/v1/images/${imageData.id}/breeds`, {
+          headers: {
+            'Content-Type': 'application/json',
+            'x-api-key': process.env.REACT_APP_CAT_API_KEY
           }
         });
-        const { breeds } = response.data[0];
-        if (breeds.length > 0) {
-          const breed = breeds[0];
-          setBreedName(breed.name);
-          setTemperament(breed.temperament);
-          setOrigin(breed.origin);
-        }
-        setCatImage(response.data[0].url); // Assuming the response contains an array of objects with a 'url' property
+
+        const catData = catDataResponse.data[0];
+        setCatData({
+          name: catData.name,
+          weight: catData.weight.imperial,
+          origin: catData.origin
+        });
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
 
-    fetchcatImage();
+    fetchCatData();
   }, []);
 
+  const newSet = async () => {
+    try {
+      const imageDataResponse = await axios.get('https://api.thecatapi.com/v1/images/search?&has_breeds=true&limit=1', {
+        headers: {
+          'Content-Type': 'application/json',
+          'x-api-key': process.env.REACT_APP_CAT_API_KEY
+        },
+      });
+
+      const imageData = imageDataResponse.data[0];
+      setCatImage(imageData);
+
+      if (!imageData) return;
+
+      const catDataResponse = await axios.get(`https://api.thecatapi.com/v1/images/${imageData.id}/breeds`, {
+        headers: {
+          'Content-Type': 'application/json',
+          'x-api-key': process.env.REACT_APP_CAT_API_KEY
+        }
+      });
+
+      const catData = catDataResponse.data[0];
+      setCatData({
+        name: catData.name,
+        weight: catData.weight.imperial,
+        origin: catData.origin
+      });
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   return (
-    <div>
-      <h1>Random cat Image</h1>
-      <img src={catImage} alt="Random Dog" />
-      <div>
-        <h2>Breed: {breedName}</h2>
-        <p>Temperament: {temperament}</p>
-        <p>Origin: {origin}</p>
+    <div className='infoDisplay'>
+      <h1>Random Cat Information</h1>
+      <div className='infoCard'>
+        {catData && (
+          <div>
+            <p><span className="infoLabel">Name: {catData.name}</span></p>
+            <p><span className="infoLabel">Weight: {catData.weight}</span></p>
+            <p><span className="infoLabel">Origin: {catData.origin}</span></p>
+          </div>
+        )}
+      </div>
+      <img src={catImage.url} alt="Random Cat" style={{ height: '250px', width: '250px' }} />
+      <div className="buttonContainer">
+        <button className='buttonPress' onClick={newSet}>Press For a Different One</button>
       </div>
     </div>
   );
